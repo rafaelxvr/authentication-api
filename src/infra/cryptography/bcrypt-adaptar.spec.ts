1f16868cc845314ea3fd33380e01bfa0683da800
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-12 11:21:10 -0300

    test: ensure Bcrypt throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adaptar.spec.ts
+++ src/infra/cryptography/bcrypt-adaptar.spec.ts
@@ -27,4 +27,14 @@ describe('BCrypt Adapter', () => {
 
     expect(hash).toBe('hash')
   })
+
+  test('Should throws if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+
+    void expect(promise).rejects.toThrow()
+  })
 })
