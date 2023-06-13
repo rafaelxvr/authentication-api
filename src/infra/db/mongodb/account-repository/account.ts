8cecf320617ca0eb3718c9fd922b794b243c98b4
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-13 08:20:18 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,15 +1,20 @@
 import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { type AccountModel } from '../../../../domain/models/account'
+import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const accountId = result.insertedId.id[0]
+
     return await new Promise(resolve => {
       resolve({
-        id: 'any_id',
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password'
+        id: accountId.toString(),
+        name: accountData.name,
+        email: accountData.email,
+        password: accountData.password
       })
     })
   }
