59d40a57fe3d985ade24316e66e7e00e824ad983
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-14 13:46:07 -0300

    feat: ensure requests parse body as json

---
+++ src/main/config/middlewares.ts
@@ -0,0 +1,5 @@
+import { type Express } from 'express'
+import { bodyParser } from '../middlewares/body-parser'
+export default (app: Express): void => {
+  app.use(bodyParser)
+}
---
+++ src/main/server.ts
@@ -0,0 +1,4 @@
+import app from './config/app'
+
+const PORT = 6060
+app.listen(PORT, () => { console.log(`Server running at http://localhost:${PORT}`) })
