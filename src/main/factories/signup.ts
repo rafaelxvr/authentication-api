2b275c014bdf9e78bc456f2ed110ce0b31f48fdd
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-15 20:43:11 -0300

    feat: add SignUpFactory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usescases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const bCryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bCryptAdapter, accountMongoRepository)
+
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
