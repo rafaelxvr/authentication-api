e724dc3d57ef6891bc125e1ba98c199c71fdaf08
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-22 10:26:38 -0300

    refactor: remove unused import

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { type HttpResponse, type HttpRequest, type Controller, type EmailValidator, type AddAccount, type Validation } from './signup-protocols'
-import { MissingParamError, InvalidParamError } from '../../errors/'
+import { InvalidParamError } from '../../errors/'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
@@ -19,14 +19,6 @@ export class SignUpController implements Controller {
 
       if (error) {
         return badRequest(error)
-      }
-
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
       }
 
       const { name, email, password, passwordConfirmation } = httpRequest.body
