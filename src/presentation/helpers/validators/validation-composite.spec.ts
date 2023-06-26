68ebe6e0cf50bc38b876bfec11163777f2ca50c6
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-26 16:34:25 -0300

    refactor: move ValidationStub to a factory helper method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,20 +2,33 @@ import { MissingParamError } from '../../errors'
 import { type Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
-const makeSut = (): ValidationComposite => {
+const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
       return new MissingParamError('field')
     }
   }
-  const validationStub = new ValidationStub()
 
-  return new ValidationComposite([validationStub])
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    validationStub,
+    sut
+  }
 }
 
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const error = sut.validate({ field: 'any_value' })
 
     expect(error).toEqual(new MissingParamError('field'))
