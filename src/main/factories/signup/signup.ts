2ebf56952917493266b76ae5db21048580dfcd86
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-26 17:44:07 -0300

    fix: fix imports

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,10 +1,10 @@
-import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { DbAddAccount } from '../../data/usescases/add-account/db-add-account'
-import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
-import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
-import { LogControllerDecorator } from '../decorators/log'
-import { type Controller } from '../../presentation/protocols'
+import { DbAddAccount } from '../../../data/usescases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { SignUpController } from '../../../presentation/controllers/signup/signup'
+import { type Controller } from '../../../presentation/protocols'
+import { LogControllerDecorator } from '../../decorators/log'
 import { makeSignUpValidation } from './signup-validation'
 
 export const makeSignUpController = (): Controller => {
