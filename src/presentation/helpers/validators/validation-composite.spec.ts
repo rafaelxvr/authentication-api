57308aa2c389dc385ba5e4d9efa30103aa88627e
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-06-26 16:36:28 -0300

    refactor: mock ValidationStub failed return

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -5,7 +5,7 @@ import { ValidationComposite } from './validation-composite'
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
-      return new MissingParamError('field')
+      return null as any
     }
   }
 
@@ -28,7 +28,9 @@ const makeSut = (): SutTypes => {
 
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const { sut } = makeSut()
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+
     const error = sut.validate({ field: 'any_value' })
 
     expect(error).toEqual(new MissingParamError('field'))
