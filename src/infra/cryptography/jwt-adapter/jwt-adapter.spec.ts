1d3b289ad347962ddf82078184eb06750adaab6c
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-03 07:56:55 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -22,4 +22,13 @@ describe('Jwt Adapter', () => {
 
     expect(accessToken).toBe('any_token')
   })
+
+  test('Should throw if sign throws', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    await expect(sut.encrypt('any_id')).rejects.toThrow()
+  })
 })
