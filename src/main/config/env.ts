10647e958c74c688552414e6999475a4d147849a
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-04 14:15:35 -0300

    refactor: change local jwtsecret

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/auth-api',
   port: process.env.PORT ?? 6060,
-  jwtSecret: process.env.JWT_SECRET ?? 'P3X@Ut#'
+  jwtSecret: process.env.JWT_SECRET ?? '4UT#4P1'
 }
