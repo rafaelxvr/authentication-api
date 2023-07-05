c030c75952a735e3ac2317162f1b76c9a8277bec
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-05 15:16:25 -0300

    chore: add docker-compose file

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:18
 WORKDIR /usr/src/auth-api
 COPY ./package.json .
 RUN npm install --omit=dev
-COPY ./dist ./dist
-EXPOSE 6061
-CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/auth-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/auth-api',
   mongoDbName: process.env.MONGO_DB_NAME ?? 'auth-api',
   port: process.env.PORT ?? 6060,
   jwtSecret: process.env.JWT_SECRET ?? '4UT#4P1'
