d977a51723db3361994ddc195998a583579b387b
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-11 16:04:05 -0300

    feat: ensure AuthMiddleware returns 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { type LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccessDeniedError } from '../errors'
-import { forbidden, ok } from '../helpers/http/http-helper'
+import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { type Middleware, type HttpRequest, type HttpResponse } from '../protocols'
 
 export class AuthMiddleware implements Middleware {
@@ -9,20 +9,24 @@ export class AuthMiddleware implements Middleware {
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
 
-    if (!accessToken) {
-      const error = forbidden(new AccessDeniedError())
-      return await Promise.resolve(error)
+      if (!accessToken) {
+        const error = forbidden(new AccessDeniedError())
+        return await Promise.resolve(error)
+      }
+
+      const account = await this.loadAccountByToken.load(accessToken)
+
+      if (!account) {
+        const error = forbidden(new AccessDeniedError())
+        return await Promise.resolve(error)
+      }
+
+      return ok({ accountId: account.id })
+    } catch (error) {
+      return serverError(error)
     }
-
-    const account = await this.loadAccountByToken.load(accessToken)
-
-    if (!account) {
-      const error = forbidden(new AccessDeniedError())
-      return await Promise.resolve(error)
-    }
-
-    return ok({ accountId: account.id })
   }
 }
