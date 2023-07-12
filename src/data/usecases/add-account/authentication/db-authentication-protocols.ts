3a91c1b7c98138bc5c88f683fd4f52490cf622df
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-11 21:15:57 -0300

    refactor: move authentication folder

--- src/data/usecases/add-account/authentication/db-authentication-protocols.ts
+++
@@ -1,6 +0,0 @@
-export * from '../../../../domain/usecases/authentication'
-export * from '../../../protocols/db/account/load-account-by-email-repository'
-export * from '../db-add-account-protocols'
-export * from '../../../protocols/cryptography/hash-comparer'
-export * from '../../../protocols/cryptography/encrypter'
-export * from '../../../protocols/db/account/update-access-token-repository'
--- src/main/factories/usecases/authentication/db-authentication-factory.ts
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -1,5 +1,5 @@
 import env from '../../../config/env'
-import { DbAuthentication } from '../../../../data/usecases/add-account/authentication/db-authentication'
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
 import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
 import { BcryptAdapter } from '../../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '../../../../infra/cryptography/jwt-adapter/jwt-adapter'
