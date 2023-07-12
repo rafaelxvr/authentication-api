8be32150c721ca850376ab4b5568d8684960a9df
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-11 22:01:39 -0300

    test: ensure DbLoadAccountByToken returns null if Decrypter returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,19 +1,47 @@
 import { type Decrypter } from '../../protocols/cryptography/decrypter'
-import { DbLoadAccountByToken } from './db-load-account-by-token.'
+import { DbLoadAccountByToken } from './db-load-account-by-token'
+
+const makeDecrypterStub = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt (id: string): Promise<string> {
+      return await Promise.resolve('any_value')
+    }
+  }
+
+  return new DecrypterStub()
+}
+
+interface SutTypes {
+  sut: DbLoadAccountByToken
+  decrypterStub: Decrypter
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypterStub()
+  const sut = new DbLoadAccountByToken(decrypterStub)
+
+  return {
+    decrypterStub,
+    sut
+  }
+}
 
 describe('DbLoadAccountByToken Usecase', () => {
   test('Should call Decrypter with correct values', async () => {
-    class DecrypterStub implements Decrypter {
-      async decrypt (id: string): Promise<string> {
-        return await Promise.resolve('any_value')
-      }
-    }
-
-    const decrypterStub = new DecrypterStub()
+    const { sut, decrypterStub } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    const sut = new DbLoadAccountByToken(decrypterStub)
-    await sut.load('any_token')
+
+    await sut.load('any_token', 'any_role')
 
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+
+  test('Should return null if Decrypter returns null', async () => {
+    const { sut, decrypterStub } = makeSut()
+    jest.spyOn(decrypterStub, 'decrypt').mockReturnValueOnce(Promise.resolve(null as any))
+
+    const account = await sut.load('any_token', 'any_role')
+
+    expect(account).toBe(null)
   })
 })
