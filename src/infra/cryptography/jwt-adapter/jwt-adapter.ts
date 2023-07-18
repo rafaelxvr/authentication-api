200f2af7b42e0f818d0f111cbf339450f03822bf
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-17 22:32:24 -0300

    feat: ensure JwtAdapter calls verify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,12 +1,19 @@
 import jwt from 'jsonwebtoken'
 import { type Encrypter } from '../../../data/protocols/cryptography/encrypter'
+import { type Decrypter } from '../../../data/protocols/cryptography/decrypter'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) { }
 
   async encrypt (value: string): Promise<string> {
     const accessToken = jwt.sign({ id: value }, this.secret)
 
     return accessToken
+  }
+
+  async decrypt (value: string): Promise<string> {
+    jwt.verify(value, this.secret)
+
+    return null as any
   }
 }
