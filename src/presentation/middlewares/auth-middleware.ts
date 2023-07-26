19422445851b5a36e4f94d1e77990774c31a9f2d
Rafael Xavier <78372916+rafaelxvr@users.noreply.github.com>
2023-07-25 21:41:31 -0300

    feat: ensure AuthMiddleware returns 403 if role is application and no x-api-key exists in headers

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { type LoadAccountByToken, type HttpRequest, type HttpResponse, type Middleware } from './auth-middlewares-protocols'
-import { AccessDeniedError } from '../errors'
-import { forbidden, ok, serverError } from '../helpers/http/http-helper'
+import { AccessDeniedError, UnauthorizedError } from '../errors'
+import { forbidden, ok, serverError, badRequest } from '../helpers/http/http-helper'
 
 export class AuthMiddleware implements Middleware {
   constructor (
@@ -13,24 +13,34 @@ export class AuthMiddleware implements Middleware {
       const accessToken = httpRequest.headers?.['x-access-token']
       const apiKey = httpRequest.headers?.['x-api-key']
 
-      if (this.role === 'application' && !apiKey) {
+      if (!accessToken || (!apiKey && this.role === 'application')) {
         const error = forbidden(new AccessDeniedError())
         return await Promise.resolve(error)
       }
 
-      if (!accessToken) {
-        const error = forbidden(new AccessDeniedError())
-        return await Promise.resolve(error)
+      if (accessToken && !apiKey) {
+        const account = await this.loadAccountByToken.load(accessToken, this.role)
+
+        if (!account) {
+          const error = forbidden(new AccessDeniedError())
+          return await Promise.resolve(error)
+        }
+
+        return ok({ accountId: account.id })
       }
 
-      const account = await this.loadAccountByToken.load(accessToken, this.role)
+      if ((apiKey && this.role === 'application') && !accessToken) {
+        const account = await this.loadAccountByToken.load(apiKey, this.role)
 
-      if (!account) {
-        const error = forbidden(new AccessDeniedError())
-        return await Promise.resolve(error)
+        if (!account) {
+          const error = forbidden(new AccessDeniedError())
+          return await Promise.resolve(error)
+        }
+
+        return ok({ accountId: account.id })
       }
 
-      return ok({ accountId: account.id })
+      return badRequest(new UnauthorizedError())
     } catch (error) {
       return serverError(error)
     }
